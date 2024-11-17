1c193a2a37f07bb120a198643abb49c500f15179
Waldemar Petrash <116195123+Scrudgie-Meyer@users.noreply.github.com>
2024-11-17 20:30:05 +0200

    Init Lab 4

---
+++ Lab4/LabExecuter.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab4;
+
+public class LabExecutor(string? lab, string inputPath, string outputPath)
+{
+    public void Execute()
+    {
+
+    }
+
+
+}
---
+++ Lab4/Program.cs
@@ -0,0 +1,22 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+
+namespace Lab4;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CommandLineApplication.Execute<Program>(args);
+    }
+    private void OnExecute()
+    {
+        Console.WriteLine("Available commands:");
+
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("  dotnet run {labName}   - Run the specified lab.");
+        Console.WriteLine("  dotnet run version     - Display app version.");
+        Console.WriteLine("  dotnet run set-path    - Set default lab directory path.");
+        Console.ResetColor();
+    }
+
+}
